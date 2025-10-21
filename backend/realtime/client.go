29d8bba0a53f7a236954da28debaff502cbda714
YashmitVaish <yashmitvaish4454@gmail.com>
2025-10-21 23:51:43 +0530

    suggested changes

--- backend/realtime/client.go
+++ backend/realtime/client.go
@@ -3,6 +3,7 @@ package realtime
 import (
 	"log"
 	"github.com/gorilla/websocket"
+	"time"
 )
 
 type Client struct {
@@ -17,6 +18,14 @@ func (c *Client) ReadPump() {
 		c.Conn.Close()
 	}()
 
+	c.Conn.SetReadLimit(512)
+	c.Conn.SetReadDeadline(time.Now().Add(60 * time.Second)) // 60s timeout
+	c.Conn.SetPongHandler(func(string) error {
+		c.Conn.SetReadDeadline(time.Now().Add(60 * time.Second))
+		return nil
+	})
+
+
 	for {
 		_, _, err := c.Conn.ReadMessage()
 		if err != nil {
@@ -26,11 +35,32 @@ func (c *Client) ReadPump() {
 }
 
 func (c *Client) WritePump() {
-	defer c.Conn.Close()
-	for msg := range c.Send {
-		if err := c.Conn.WriteMessage(websocket.TextMessage, msg); err != nil {
-			log.Println("write error:", err)
-			break
-		}
-	}
+    ticker := time.NewTicker(30 * time.Second)
+    defer func() {
+        ticker.Stop()
+        c.Conn.Close()
+    }()
+
+    for {
+        select {
+        case msg, ok := <-c.Send:
+            c.Conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+            if !ok {
+                c.Conn.WriteMessage(websocket.CloseMessage, []byte{})
+                return
+            }
+            if err := c.Conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+                log.Println("write error:", err)
+                return
+            }
+
+        case <-ticker.C:
+            c.Conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+            if err := c.Conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+                log.Println("ping error:", err)
+                return
+            }
+        }
+    }
 }
+
