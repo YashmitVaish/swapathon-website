cdc147b645e6c4daa16aa8703d97f52bbf03fc7f
Vansh Wadhwa <vanshreads@gmail.com>
2025-10-23 11:46:47 +0530

    Added website dir with landing page

---
+++ website/src/pages/Homepage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import grass from '../assets/grassSamurai.png'
+
+function Homepage() {
+  return (
+    <div className="min-h-screen">
+      <div className='flex justify-center items-center'>
+        <img 
+          src={grass} 
+          alt="Grass Samurai"
+          className="max-w-full h-auto" 
+        />
+        <div>
+            efjkn
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
