a46176c658d038ed431106df1150ced2e0fd8300
Vansh Wadhwa <vanshreads@gmail.com>
2025-10-24 16:33:33 +0530

    Landing page completed

--- website/src/pages/Homepage.jsx
+++ website/src/pages/Homepage.jsx
@@ -6,14 +6,24 @@ import grass from '../assets/grassSamurai.png'
 function Homepage() {
   return (
      <div className="relative h-screen w-screen overflow-hidden">
-   
-      <img
+      <img  
         src={branches}
         alt="branches"
-        className="absolute  z-10 max-w-[30%] md:max-w-[20%] object-contain"
+        className="absolute  z-10 max-w-[30%] md:max-w-[20%] "
       />
+      <div className='flex justify-end'>
 
-      <div className="absolute inset-0 flex items-center justify-center z-20 pointer-events-none">
+      <div className='flex flex-col'>
+        <div className="font-lastshuriken  mr-30 text-4xl mt-30">
+         Feature Creep Chaos
+        </div>
+         <div className='font-zenantique flex justify-end mr-30 text-3xl mt-1'>
+          武の心
+          </div>
+          </div>
+      </div>
+
+      <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
         <div className="relative flex items-center justify-center">
           <img
             src={banner}
@@ -23,7 +33,7 @@ function Homepage() {
           <img
             src={Seal}
             alt="seal"
-            className="absolute object-contain z-30"
+            className="absolute"
           />
         </div>
       </div>
@@ -31,7 +41,7 @@ function Homepage() {
       <img
         src={grass}
         alt="grass"
-        className="absolute bottom-0 left-0 w-full object-cover z-15"
+        className="absolute bottom-0 left-0 w-full h-100 z-10"
       />
     </div>
   )
