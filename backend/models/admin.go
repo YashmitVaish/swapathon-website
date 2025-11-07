20137782cfeb5d42ac1555352181ccc3de1f11b2
YashmitVaish <yashmitvaish4454@gmail.com>
2025-11-07 12:04:02 +0530

    index fix

--- backend/models/admin.go
+++ backend/models/admin.go
@@ -5,6 +5,6 @@ import "gorm.io/gorm"
 type Admin struct {
 	gorm.Model
 	Username string `gorm:"unique;not null"`
-	Password string `gorm:"not null"` // hashed later
+	Password string `gorm:"not null"` //hash and add to DB
 	Email    string `gorm:"unique"`
 }
--- backend/models/team.go
+++ backend/models/team.go
@@ -6,7 +6,7 @@ import (
 
 type Team struct {
 	ID               uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	TeamName         string    `gorm:"unique;not null"`
+	TeamName         string    `gorm:"unique;not null;index"`
 	LeaderName       string    `gorm:"not null"`
 	Email            string    `gorm:"unique;not null;index"`
 	PasswordHash     string    `gorm:"not null"`
