bf24b7da92aa95e4364d3813405b7a0c86d3283e
YashmitVaish <yashmitvaish4454@gmail.com>
2025-11-11 15:34:26 +0530

    fix db

--- backend/models/user.go
+++ backend/models/user.go
@@ -6,7 +6,7 @@ import (
 
 type User struct {
 	gorm.Model
-	Name        string `gorm:"unique;not null"`
+	Name        string `gorm:"not null"`
 	Email       string `gorm:"unique;not null"`
 	RollNumber  int    `gorm:"unique;not null"`
 	PhoneNumber int    `gorm:"unique;not null"`
